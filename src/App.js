11e923e0e82b7aafa5ac1efbc837ac33ef02c439
THIAGO EGGRES <thiago.eggres@idwall.co>
2024-03-15 09:35:28 -0300

    created knight movement

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import { HexGrid, Layout, Hexagon, GridGenerator, Pattern } from 'react-hexgrid';
 import './App.css';
+import GridStateGenerator from './board/gridStateGenerator';
 
 class App extends Component {
   render() {
-    const hexagons = GridGenerator.hexagon(3).filter(hex => !(hex.q==0 && hex.r==0 && hex.s==0));
 
     return (
       <div className="App">
-        <h1>Basic example of HexGrid usage.</h1>
-        <HexGrid width={1200} height={1000}>
-          <Layout size={{ x: 7, y: 7 }}>
-            { hexagons.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
-            <Hexagon key={999} q={0} r={0} s={0} fill="knight-pat"/>
-          </Layout>
-          <Pattern id="knight-pat" link="https://www.iconpacks.net/icons/2/free-knight-helmet-icon-2747-thumb.png" size={{x:7, y:7}} />
-        </HexGrid>
+        <GridStateGenerator/>
       </div>
     );
   }
